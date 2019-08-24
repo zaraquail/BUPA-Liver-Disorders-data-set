b8447ce123348794d4243cd9ff97d802a3cf910b
Zara Quail <50523287+zaraquail@users.noreply.github.com>
2019-08-24 22:55:14 +0100

    Delete minimum.py
    Deleted extraneous files used for trial and error.

--- minimum.py
+++
@@ -1,6 +0,0 @@
-# Zara Quail August 2019 Assignment
-
-# Calculate the mean number in each column of the BUPA Liver Disorders data set
-# Reference: Ian McLoughlin. Matplotlib Pyplot video. 2018. https://web.microsoftstream.com/video/f0788c1c-c7bd-4347-98ac-477186938ed7 
-
-numpy.min(firstcol)
